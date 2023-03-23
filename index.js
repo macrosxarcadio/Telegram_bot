29d4bda27f999a765d51bc8aa4169fb51590fea0
German Frate <frate_g@solarsistemas.net>
2023-03-23 15:45:22 -0300

    test44

--- index.js
+++ index.js
@@ -4,38 +4,17 @@ const { google } = require('googleapis');
 var _ = require('lodash');
 require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const lock = new Lock();
 
 const auth = new google.auth.GoogleAuth({
     keyFile: 'google-api-credentials.json',
     scopes: 'https://www.googleapis.com/auth/spreadsheets'
-})
+});
 
-async function read(range, majorDimension) {
-    //Create client instance
-    const client = await auth.getClient();
-    //Instance of google sheets api
-    const googleSheets = google.sheets({ version: 'v4', auth: client });
-    const met = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId: '1Ku5VfmmmsTGDzEUoPqUQ-Hdh0bD-mmSfF4u6O6sFj8I',
-        range: `${range}`,
-        majorDimension: majorDimension ? majorDimension : null,
-    });
-    return met.data.values;
-}
-
-async function readBalance() {
-    // funciona como un buscar V
-    let balanceRow = await read('hoja_2!A:A', 'COLUMNS');
-    let row = await balanceRow[0].indexOf('Saldo', 0);
-    let balance = await read(`hoja_2!D${row + 1}`, 'COLUMNS');
-    return balance;
-}
-//t//
 async function write(data) {
     //Create client instance
+    const client = await auth.getClient();
     try {
-        const client = auth.getClient();
         //Instance of google sheets api
         const googleSheets = google.sheets({ version: 'v4', auth: await client });
         const writing = googleSheets.spreadsheets.values.append({
@@ -50,25 +29,6 @@ async function write(data) {
         console.log(error);
     }
 }
-
-bot.command('fondo', (ctx) => {
-    readBalance().then((response) => {
-        ctx.reply(response.flat()[0]);
-    }
-    )
-}
-)
-bot.command('r', (ctx) => {
-    read().then((response) => {
-        ctx.reply(response.values);
-        response.values.map(r => ctx.reply(r));
-    }
-    )
-})
-
-bot.command('test', (ctx) => {
-    ctx.reply('el bot funciona');
-})
 
 bot.command('gasto', (ctx) => {
     const regtime = moment().format('DD-MM-YYYY');
